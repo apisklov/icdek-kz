d512ebc0bc74257559d9b0d5ddcc395de307467e
apisklov <apisklov@bk.ru>
2026-04-21 19:39:47 +0300

    Feat: добавил перевод

--- inc/supports.php
+++ inc/supports.php
@@ -38,14 +38,14 @@ function add_supports()
 
 add_action('wp_footer', 'output_cookie_notice');
 
-function output_cookie_notice() {
-	if( isset( $_COOKIE['cookieNotice'] ) ) {
-		return;
-	} ?>
-	<div class="cookie-notice">
-		<div class="cookie-notice__text">Пользуясь нашим сайтом, вы соглашаетесь с тем, что мы используем cookies.</div>
-		<div class="cookie-notice__buttons">
-			<a href="#" class="cookie-notice__confirm">Принять</a>
-		</div>
-	</div>
+function output_cookie_notice()
+{
+    if (! isset($_COOKIE['cookieNotice'])) { ?>
+        <div class="cookie-notice">
+            <div class="cookie-notice__text"><?php echo __( 'Пользуясь нашим сайтом, вы соглашаетесь с тем, что мы используем cookies', 'icdek' ) ?>.</div>
+            <div class="cookie-notice__buttons">
+                <a href="#" class="cookie-notice__confirm"><?php echo __( 'Принять', 'icdek' ) ?></a>
+            </div>
+        </div>
+    <?php } ?>
 <?php }
